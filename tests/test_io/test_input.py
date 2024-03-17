42b9491480a789d5db92ad55a4407ba321c885cc
Mariia Shpir <99517424+shpirm@users.noreply.github.com>
2024-03-17 23:14:53 +0200

    added test2 for input

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -10,3 +10,10 @@ class TestInput(unittest.TestCase):
             file.write(expected)
         result = read_from_file_builtin(TestInput.data_source + "test.txt")
         self.assertEqual(result, expected)
+
+    def test_read_from_file_pandas(self):
+        expected = "Test text"
+        with open(TestInput.data_source + "test.csv", "w") as file:
+            file.write(expected)
+        result = read_from_file_pandas(TestInput.data_source + "test.csv")
+        self.assertEqual(result.strip(), expected)
